d931127d41a06aaae5a35c9bef30bb842016f72c
Zhihao Yang <zhihao.yang@gmx.de>
2022-11-22 23:22:28 +0100

    20221122

--- Add Two Numbers-1.py
+++ Add Two Numbers-1.py
@@ -8,14 +8,17 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode], carry = 0) -> Optional[ListNode]:
 
         val = l1.val + l2.val + carry
+        print("val: ", val)
         carry = val // 10
+        print("carry: ", carry)
         result = ListNode(val % 10)
+        print("result: ", result.val, result.next)
 
         if (l1.next != None or l2.next != None or carry != 0):
             if l1.next == None:
-                l1.next = ListNode(0)
+                l1.next = ListNode(val=0)
             if l2.next == None:
-                l2.next = ListNode(0)
+                l2.next = ListNode(val=0)
             result.next = self.addTwoNumbers(l1.next, l2.next, carry)
         return result
 
@@ -23,9 +26,10 @@ class Solution:
 
 
 
-Liste = ListNode()
-Solu = Solution()
-l1 = [2,4,3]
-l2 = [5,6,4]
+l1 = ListNode(val=9, next = ListNode(val = 9, next = ListNode(val = 9, next = None)))
+l2 = ListNode(val=5, next = ListNode(val = 6, next = ListNode(val = 4, next = None)))
+print([l1.val, l1.next.val,l1.next.next.val])
+print([l2.val, l2.next.val,l2.next.next.val])
 carry = 0
-print(Solu.addTwoNumbers(Optional[l1], Optional[l2], carry))
+res = Solution().addTwoNumbers(l1, l2, carry)
+print([res.val, res.next.val, res.next.next.val, res.next.next.next.val, res.next.next.next.next])
